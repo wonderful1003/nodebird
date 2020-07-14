bef42eade76f2e3e3d1a0cee3666f327875cd8e2
wonderful1003 <wisecat1003@naver.com>
2020-07-15 02:43:45 +0900

    Initial commit

---
+++ config/config.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: 'root',
+    password: process.env.SEQUELIZE_PASSWORD,
+    database: 'nodebird',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorsAliases: 'false',
+  },
+  PROD: {
+    username: 'root',
+    password: process.env.SEQUELIZE_PASSWORD,
+    database: 'nodebird',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorsAliases: 'false',
+    logging: false,
+  },
+};
